b555c705c7c1b71a729a5d6528c8f3a9185c4946
Félx <lebazardebryanoff@gmail.com>
2024-08-26 21:21:37 +0200

    Update
    generate_music_list.py

--- generate_music_list.py
+++ generate_music_list.py
@@ -5,7 +5,7 @@ def list_files(startpath):
     file_list = []
     for root, dirs, files in os.walk(startpath):
         for file in files:
-            if file.endswith('.wav', '.webm'):
+            if any(file.endswith(ext) for ext in extensions):
                 file_list.append({
                     'name': file,
                     'path': os.path.relpath(os.path.join(root, file), startpath)
@@ -13,7 +13,8 @@ def list_files(startpath):
     return file_list
 
 start_directory = '.'
-files = list_files(start_directory)
+extensions = ['.wav', '.flac']
+files = list_files(start_directory, extensions)
 
 with open('music.json', 'w') as f:
     json.dump(files, f, indent=4)
