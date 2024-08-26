8f7840fb9dc43b6f490772a9b85fbec11547fff7
Félx <85786075+LeBazarDeBryan@users.noreply.github.com>
2024-08-26 19:57:31 +0200

    Create
    generate_music_list.py

---
+++ generate_music_list.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+def list_files(startpath):
+    file_list = []
+    for root, dirs, files in os.walk(startpath):
+        for file in files:
+            if file.endswith('.wav'):
+                file_list.append({
+                    'name': file,
+                    'path': os.path.relpath(os.path.join(root, file), startpath)
+                })
+    return file_list
+
+start_directory = '.'
+files = list_files(start_directory)
+
+with open('music.json', 'w') as f:
+    json.dump(files, f, indent=4)
+
+print('Liste de musique (JSON) généré avec succès!')
