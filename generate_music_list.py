af7af4f986aa0cd5d0031157d4113b5b4d997c99
Félx <lebazardebryanoff@gmail.com>
2024-08-28 15:58:53 +0200

    Update
    [TEST] generate_music_list.py

--- generate_music_list.py
+++ generate_music_list.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-def list_files(start_directory, extensions):
+def list_files(start_directory, extensions, lyrics_directory):
     matching_files = []
     
     for root, dirs, files in os.walk(start_directory):
@@ -12,7 +12,7 @@ def list_files(start_directory, extensions):
                 title = os.path.splitext(file)[0]
                 file_name = f"{author} - {title}"
 
-                lyrics_file_path = os.path.join(lyrics, f"{title}.txt")
+                lyrics_file_path = os.path.join(lyrics_directory, f"{title}.txt")
                 lyrics = ""
                 if os.path.exists(lyrics_file_path):
                     with open(lyrics_file_path, 'r') as lyrics_file:
@@ -34,8 +34,9 @@ def update_json_file(json_file, data):
 
 start_directory = 'music'
 extensions = ['.wav', '.flac']
+lyrics_directory = 'lyrics'
 json_file = 'music.json'
 
-files = list_files(start_directory, extensions)
+files = list_files(start_directory, extensions, lyrics_directory)
 update_json_file(json_file, files)
 print(f'Fichier JSON mis à jour avec {len(files)} fichiers.')
